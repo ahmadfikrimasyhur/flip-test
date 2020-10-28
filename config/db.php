ffe3213dc4079f1e90d58a00129fd9c3e8aea1ff
ahmad fikri masyhur <ahmadfikri.m@gmail.com>
2020-10-28 17:14:43 +0700

    update db heroku

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=ec2-54-157-4-216.compute-1.amazonaws.com;port=5432;dbname=dsti081unjqb0',
-    'username' => 'jwhsibxkrcvthq',
-    'password' => 'e74762ffb8694a6be66571371492fd6b920376d1248a4703263063cfafb99d4b',
+    'dsn' => 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_0bf93b47359a953',
+    'username' => 'b15aab4928a673',
+    'password' => '88e6065a',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
