b783842035c42d43e85d31d66100d40e828f8ba8
ahmad fikri masyhur <ahmadfikri.m@gmail.com>
2020-10-28 16:47:55 +0700

    check db config

--- views/site/index.php
+++ views/site/index.php
@@ -5,6 +5,13 @@ $this->title = 'My Marketplace';
 
     <div class="jumbotron">
         <h1>Welcome back<?php echo Yii::$app->user->isGuest ? '' : ', '.Yii::$app->user->identity->username ?>!</h1>
+        <p>
+        <?php
+        $dbopts = parse_url(getenv('DATABASE_URL'));
+        
+        var_dump($dbopts);
+        ?>
+        </p>
     </div>
 
 </div>
