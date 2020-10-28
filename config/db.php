071b738182908663d99f471f8a7b3480fd786b13
ahmad fikri masyhur <ahmadfikri.m@gmail.com>
2020-10-28 17:26:56 +0700

    update db config heroku

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=flip-test',
-    'username' => 'root',
-    'password' => '123456',
+    'dsn' => 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_0bf93b47359a953',
+    'username' => 'b15aab4928a673',
+    'password' => '88e6065a',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
