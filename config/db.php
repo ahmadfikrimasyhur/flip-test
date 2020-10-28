4350578da4a6ddfa202f0687b967a83db06d5f1e
ahmad fikri masyhur <ahmadfikri.m@gmail.com>
2020-10-28 16:53:53 +0700

    update db heroku

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=flip_test',
-    'username' => 'root',
-    'password' => '123456',
+    'dsn' => 'pgsql:host=ec2-54-157-4-216.compute-1.amazonaws.com;port=5432;dbname=dsti081unjqb0',
+    'username' => 'jwhsibxkrcvthq',
+    'password' => 'e74762ffb8694a6be66571371492fd6b920376d1248a4703263063cfafb99d4b',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
